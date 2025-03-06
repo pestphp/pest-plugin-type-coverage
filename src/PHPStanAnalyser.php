ae457f5c418e02ec249080baada495ba6235acd8
Thiago Marsola <tmarsola94@gmail.com>
2025-03-05 23:23:13 -0300

    fix: Refactor FileAnalyser instantiation with named arguments
    Updated the `FileAnalyser` constructor usage to leverage named arguments for improved clarity and maintainability. Added `IgnoreErrorExtensionProvider` as a dependency to align with updated requirements.

--- src/PHPStanAnalyser.php
+++ src/PHPStanAnalyser.php
@@ -8,6 +8,7 @@ use Composer\InstalledVersions;
 use PhpParser\Node;
 use PHPStan\Analyser\Analyser;
 use PHPStan\Analyser\FileAnalyser;
+use PHPStan\Analyser\IgnoreErrorExtensionProvider;
 use PHPStan\Analyser\LocalIgnoresProcessor;
 use PHPStan\Analyser\NodeScopeResolver;
 use PHPStan\Analyser\RuleErrorTransformer;
@@ -145,12 +146,13 @@ final class PHPStanAnalyser
         }
 
         $fileAnalyser = new FileAnalyser(
-            $scopeFactory,
-            $nodeScopeResolver,
-            $container->getService('defaultAnalysisParser'), // @phpstan-ignore-line
-            $container->getByType(DependencyResolver::class),
-            new RuleErrorTransformer,
-            $container->getByType(LocalIgnoresProcessor::class),
+            scopeFactory: $scopeFactory,
+            nodeScopeResolver: $nodeScopeResolver,
+            parser: $container->getService('defaultAnalysisParser'), // @phpstan-ignore-line
+            dependencyResolver: $container->getByType(DependencyResolver::class),
+            ignoreErrorExtensionProvider: $container->getByType(IgnoreErrorExtensionProvider::class),
+            ruleErrorTransformer: new RuleErrorTransformer,
+            localIgnoresProcessor: $container->getByType(LocalIgnoresProcessor::class),
         );
 
         return new Analyser($fileAnalyser, $ruleRegistry, $collectorRegistry, $nodeScopeResolver, 9_999_999_999_999);
